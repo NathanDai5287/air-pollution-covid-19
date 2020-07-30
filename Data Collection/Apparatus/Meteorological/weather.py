58d13642b450ad30afb92b33085e7ffd6f1191fd
Nathan Dai <nathandai2000@gmail.com>
2020-07-30 12:15:02 -0700

    multi threading but ran out of api key uses

--- Data Collection/Apparatus/Meteorological/weather.py
+++ Data Collection/Apparatus/Meteorological/weather.py
@@ -1,8 +1,7 @@
 from wwo_hist import retrieve_hist_data
 import pandas as pd
 import os
-import sys
-
+import concurrent.futures
 
 """
 ['date_time', 'maxtempC', 'mintempC', 'totalSnow_cm', 'sunHour',
@@ -14,31 +13,42 @@ import sys
 
 
 # api key, hours, start: "01-Jan-2020", end: "01-Jan-2021", ["92130"]
-def weather(api_key: str, frequency: int, start_date: str, end_date: str, zip_code: list, location_label=False, export_csv=True, store_df=True) -> pd.DataFrame:
+def weather(start_date: str, end_date: str, zip_code: str, frequency: int, location_label=False, export_csv=True, store_df=True, api_key='62c4f496efb147c1b2160953202406') -> pd.DataFrame:
     """returns a pandas DataFrame that contains:
     location, date, average temperature, humidity, wind speed, air pressure, uvIndex
 
     Args:
-        api_key (str): obtained from website; valid for 90 daya
-        frequency (int): how frequent the data should be in hours, usually 24
-        start_date (str): start date inclusive
-        end_date (str): end date inclusive
-        zip_code (list): zip code to get data from
-        location_label (bool, optional): no clue what this does. Defaults to False.
-        export_csv (bool, optional): [description]. Defaults to False.
-        store_df (bool, optional): [description]. Defaults to True.
+        start_date (str): start date inclusive; '01-Jan-2020'
+        end_date (str): end date inclusive; '02-Jan-2020'
+        zip_code (list): list of zip codes
+        frequency (int): frequency in hours, usually 24
+        location_label (bool, optional): no clue. Defaults to False.
+        export_csv (bool, optional): keep as True. Defaults to True.
+        store_df (bool, optional): keep as True. Defaults to True.
+        api_key (str, optional): obtained from website; valid for 90 days. Defaults to '62c4f496efb147c1b2160953202406'.
 
     Returns:
-        pd.DataFrame: [description]
+        pd.DataFrame: contains data listed above
     """
 
-    retrieve_hist_data(api_key, zip_code, start_date, end_date, frequency,
-                       location_label=location_label, export_csv=export_csv, store_df=store_df)
+    retrieve_hist_data(api_key, [zip_code], start_date, end_date, frequency,
+                       location_label=location_label, export_csv=True, store_df=store_df)
 
-    data = pd.read_csv(zip_code[0] + '.csv', header=0)
-    os.remove(zip_code[0] + '.csv')
+    data = pd.read_csv(zip_code + '.csv', header=0)
+    os.remove(zip_code + '.csv')
+
+    print(f'deleted file {zip_code}.csv')
 
     return data[['location', 'date_time', 'tempC', 'humidity', 'windspeedKmph', 'pressure', 'uvIndex']] # choose which to return
+
+def complete_weather_procces(start_date: str, end_date: str, zip_code: str, frequency: int, location_label=False, export_csv=True, store_df=True, api_key='62c4f496efb147c1b2160953202406') -> bool:
+    data = weather(start_date, end_date, zip_code, frequency)
+
+    with open(r'Data Collection\Data\Meteorological\\' + zip_code + '.csv', 'w', newline='') as f:
+        f.write(data.to_csv())
+        print(f'write {zip_code}')
+    
+    return True
 
 if __name__ == '__main__':
 
@@ -47,20 +57,15 @@ if __name__ == '__main__':
     frequency = 24  # hours
     start_date = '01-JAN-2019'
     end_date = '31-DEC-2019'
+    # end_date = '31-JAN-2019'
     api_key = API_KEY
     # location_list = ['92130'] # list of zip codes to get data from
 
     # data = weather(api_key, frequency, start_date, end_date,
     #             location_list, store_df=True, export_csv=True)
 
-
     with open(r'Data Collection\Apparatus\Docs\all_zip_codes.csv') as f:
         all_zip_codes = [code.strip().strip(',') for code in f.readlines()]
-
-    for code in all_zip_codes:
-        data = weather(api_key, 24, start_date, end_date, [code])
-        print(data)
-
-        with open(r'Data Collection\Data\Meteorological\\' + code + '.csv', 'w', newline='') as f:
-            f.write(data.to_csv())
-        
+    complete_weather_procces(start_date, end_date, all_zip_codes[40], 24)
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+        # _ = [executor.submit(complete_weather_procces, start_date, end_date, code, 24) for code in all_zip_codes[40:41]]
