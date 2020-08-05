1c96de57357d1d9c3e350c9cbb7529abce130b59
Nathan Dai <nathandai2000@gmail.com>
2020-08-05 14:13:21 -0700

    spearman correlation

--- Data Analysis/correlation_table.py
+++ Data Analysis/correlation_table.py
@@ -4,4 +4,4 @@ df = pd.read_csv('Data Analysis\combined_data.csv')
 df.drop(['date_local'], axis=1)
 
 with open('Data Analysis\correlation_table.csv', 'w', newline='') as f:
-    f.write(df.corr('pearson').to_csv())
+    f.write(df.corr('spearman').to_csv())
--- Data Collection/Apparatus/Meteorological/weather.py
+++ Data Collection/Apparatus/Meteorological/weather.py
@@ -1,7 +1,13 @@
 from wwo_hist import retrieve_hist_data
 import pandas as pd
+import sys
 import os
 import concurrent.futures
+import datetime
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+
+from top_counties import most_infected
 
 """
 ['date_time', 'maxtempC', 'mintempC', 'totalSnow_cm', 'sunHour',
@@ -12,7 +18,7 @@ import concurrent.futures
 """
 
 
-def weather(start_date: str, end_date: str, zip_codes: list, frequency: int, api_key='272f03c5e20641598f4200659203007') -> None:
+def weather(start_date: str, end_date: str, zip_codes: list, frequency: int, api_key='03347c11e286435386945637203107') -> None:
     """writes to a csv file in the meteorological data directory containing 1 year's worth of meteorological data
 
     Args:
@@ -20,7 +26,7 @@ def weather(start_date: str, end_date: str, zip_codes: list, frequency: int, api
         end_date (str): end date inclusive; '02-Jan-2020'
         zip_code (list): list of zip codes
         frequency (int): frequency in hours, usually 24
-        api_key (str, optional): [description]. Defaults to '272f03c5e20641598f4200659203007'.
+        api_key (str, optional): obtained from website. Defaults to 'd10267c436334ca88d5222321203007'.
     """
 
     retrieve_hist_data(api_key, zip_codes, start_date, end_date, frequency,
@@ -38,19 +44,25 @@ def weather(start_date: str, end_date: str, zip_codes: list, frequency: int, api
 
 if __name__ == '__main__':
 
-    API_KEY = '272f03c5e20641598f4200659203007' # max 500 requests per day
+    API_KEY = '03347c11e286435386945637203107' # max 500 requests per day
 
     frequency = 24  # hours
-    start_date = '01-JAN-2019'
-    end_date = '31-DEC-2019'
-    # end_date = '02-JAN-2019'
+    # start_date = '01-JAN-2019'
+    start_date = datetime.date(2020, 4, 1)
+    str_start_date = '01-APR-2020'
+    # end_date = '31-DEC-2019'
+    end_date = datetime.date(2020, 5, 31)
+    str_end_date = '31-MAY-2020'
     api_key = API_KEY
 
-    with open(r'Data Collection\Apparatus\Docs\all_zip_codes.csv') as f:
-        all_zip_codes = [code.strip().strip(',') for code in f.readlines()]
-
     # use this to test if all requests have been used
-    # weather(start_date, end_date, all_zip_codes[429:430], 24)
+    # weather(start_date, end_date, ['60004'], 24)
+
+    # with concurrent.futures.ThreadPoolExecutor() as executor:
+        # executor.submit(weather, str_start_date, str_end_date, ['60001'], 24)
+    # exit(0)
+    
+    zip_codes = most_infected(5, start_date, True)
 
     with concurrent.futures.ThreadPoolExecutor() as executor:
-        _ = [executor.submit(weather, start_date, end_date, [code], 24) for code in all_zip_codes]
+        _ = [executor.submit(weather, str_start_date, str_end_date, [str(code)], 24) for code in zip_codes]
